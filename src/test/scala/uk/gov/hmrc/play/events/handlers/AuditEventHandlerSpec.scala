eb51cffc12c3ef9b0632d4f3e1c75d7719e3733d
SeanMurray1 <sean.murray@digital.hmrc.gov.uk>
2021-05-07 14:43:07 +0100

    [SM][DLS-4171] Updating librarys as well as copyright year. (#22)
    Signed-off-by: Sean Murray <sean.murray@digital.hmrc.gov.uk>

--- src/test/scala/uk/gov/hmrc/play/events/handlers/AuditEventHandlerSpec.scala
+++ src/test/scala/uk/gov/hmrc/play/events/handlers/AuditEventHandlerSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 HM Revenue & Customs
+ * Copyright 2021 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +18,7 @@ package uk.gov.hmrc.play.events.handlers
 
 import org.mockito.Mockito._
 import org.scalatest.WordSpec
-import org.scalatest.mock.MockitoSugar
+import org.scalatestplus.mockito.MockitoSugar
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.play.audit.http.connector.AuditConnector
 import uk.gov.hmrc.play.events.Loggable
