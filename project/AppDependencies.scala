9b5f4033510d05316f299086a05168e4bed16b6b
luke.jones <47660116+BetaDraconis@users.noreply.github.com>
2024-01-08 16:52:29 +0000

    DLS-9714: Update all dependencies and plugins + minor refactoring of build.sbt

---
+++ project/AppDependencies.scala
@@ -0,0 +1,18 @@
+import sbt._
+
+object AppDependencies {
+  private val playVersion = "play-30"
+  private val bootstrapVersion = "8.4.0"
+
+  val compile: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc" %% s"http-verbs-$playVersion" % "14.12.0",
+    "uk.gov.hmrc" %% s"play-auditing-$playVersion" % "8.8.0"
+  )
+
+  lazy val test: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc" %% s"bootstrap-test-$playVersion" % bootstrapVersion,
+    "org.scalatestplus"  %% "mockito-4-2"  % "3.2.11.0"
+  ).map(_ % "test, it")
+
+  val all: Seq[sbt.ModuleID] = compile ++ test
+}
